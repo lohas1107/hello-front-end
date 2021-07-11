ad0d038914aeb37f5df0fb6d03c956c72cea8482
Irene Wang <lohas1107@gmail.com>
2021-07-11 12:14:34 +0800

    introduce gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')({lazy: false});
+const browserSync = require('browser-sync').create();
+
+const staticFiles = [
+    "tools/**/*",
+    "resume/**/*",
+    "pixel-perfect-kata/**/*",
+    "dessert/**/*"
+];
+const distPath = './dist';
+
+function clean() {
+    return gulp.src(distPath, {
+        read: false,
+        allowEmpty: true,
+    })
+        .pipe($.clean());
+}
+
+function copyFile() {
+    return gulp.src(staticFiles, {base: './'})
+        .pipe(gulp.dest(distPath))
+        .pipe(
+            browserSync.reload({
+                stream: true,
+            }),
+        );
+}
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: distPath,
+        },
+        port: 8080,
+    });
+}
+
+function watch() {
+    gulp.watch(staticFiles, gulp.series(copyFile));
+}
+
+function deploy() {
+    return gulp.src(`${distPath}/**/*`)
+        .pipe($.ghPages());
+}
+
+exports.default = gulp.series(clean, copyFile, gulp.parallel(browser, watch));
+
+exports.clean = clean;
+exports.build = gulp.series(clean, copyFile);
+exports.deploy = deploy;
